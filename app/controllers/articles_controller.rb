b51b9376ed719912cb2df9f3fe8feb152b38ca58
skalynch <skalynch@gmail.com>
2016-02-19 00:31:26 +0000

    Delete action added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,13 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article was deleted"
+        redirect_to articles_path
+    end
+    
     def update
         @article = Article.find(params[:id])
         if @article.update(article_params)
@@ -36,6 +43,7 @@ class ArticlesController < ApplicationController
             render 'edit'
         end
     end
+    
     
     
     private
